e25cccb8bc98e5f34d2cf9bb433373407681f2a6
georgekaran <georgekaran12@gmail.com>
2020-07-02 22:06:17 -0300

    refactor: move EmailValidation to the ValidationComposite

--- src/main/factories/signup-validation.spec.ts
+++ src/main/factories/signup-validation.spec.ts
@@ -3,10 +3,21 @@ import { ValidationComposite } from '../../presentation/helpers/validators/valid
 import { RequiredFieldValidation } from '../../presentation/helpers/validators/required-field-validation'
 import { Validation } from '../../presentation/helpers/validators/validation'
 import { CompareFieldsValidation } from '../../presentation/helpers/validators/compare-fields-validation'
+import { EmailValidation } from '../../presentation/helpers/validators/email-validation'
+import { EmailValidator } from '../../presentation/protocols/email-validator'
 
 jest.mock('../../presentation/helpers/validators/validation-composite')
 
 describe('SignUpValidation Factory', () => {
+  const makeEmailValidator = (): EmailValidator => {
+    class EmailValidatorStub implements EmailValidator {
+      isValid (email: string): boolean {
+        return true
+      }
+    }
+    return new EmailValidatorStub()
+  }
+
   test('should call ValidationComposite with all validations', () => {
     makeSignUpValidation()
     const validations: Validation[] = []
@@ -14,6 +25,7 @@ describe('SignUpValidation Factory', () => {
       validations.push(new RequiredFieldValidation(field))
     }
     validations.push(new CompareFieldsValidation('password', 'passwordConfirmation'))
+    validations.push(new EmailValidation('email', makeEmailValidator()))
     expect(ValidationComposite).toHaveBeenCalledWith(validations)
   })
 })
--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -1,4 +1,3 @@
-import { EmailValidatorAdapter } from '../../utils/email-validator-adapter'
 import { SignUpController } from '../../presentation/controllers/signup/signup'
 import { DbAddAccount } from '../../data/usecases/add-account/db-add-account'
 import { BCryptAdapter } from '../../infra/criprography/bcrypt-adapter'
@@ -10,11 +9,10 @@ import { makeSignUpValidation } from './signup-validation'
 
 export const makeSignUpController = (): Controller => {
   const salt = 12
-  const emailValidatorAdapter = new EmailValidatorAdapter()
   const bCryptAdapter = new BCryptAdapter(salt)
   const accountMongoRepository = new AccountMongoRepository()
   const dbAddAccount = new DbAddAccount(bCryptAdapter, accountMongoRepository)
-  const signUpController = new SignUpController(emailValidatorAdapter, dbAddAccount, makeSignUpValidation())
+  const signUpController = new SignUpController(dbAddAccount, makeSignUpValidation())
   const logMongoRepository = new LogMongoRepository()
   return new LogControllerDecorator(signUpController, logMongoRepository)
 }
