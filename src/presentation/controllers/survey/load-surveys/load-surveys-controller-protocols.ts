5fc138e06094140c2830760bbeb14ce8a5964ec9
georgekaran <georgekaran12@gmail.com>
2020-07-25 10:52:01 -0300

    feat: ensure LoadSurveysController calls LoadSurveysUseCase

---
+++ src/presentation/controllers/survey/load-surveys/load-surveys-controller-protocols.ts
@@ -0,0 +1,4 @@
+export * from '../../../protocols'
+export * from '../../../protocols/validation'
+export * from '../../../../domain/usecases/load-surveys'
+export * from '../../../../domain/models/survey'
