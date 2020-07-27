980a1dcb128a9a9d2e5b0e8419de64f22f147209
georgekaran <georgekaran12@gmail.com>
2020-07-27 20:01:16 -0300

    feat: ensure SurveyResultMongoRepository inser new survey result

---
+++ src/infra/db/mongodb/survey-result/survey-result-mongo-repository.ts
@@ -0,0 +1,24 @@
+import { SaveSurveyResultRepository } from '@/data/protocols/db/survey/save-survey-result-repository'
+import { SaveSurveyResultModel } from '@/domain/usecases/save-survey-result'
+import { SurveyResultModel } from '@/domain/models/survey-result'
+import { MongoHelper } from '../helpers/mongo-helper'
+
+export class SurveyResultMongoRepository implements SaveSurveyResultRepository {
+  async save (data: SaveSurveyResultModel): Promise<SurveyResultModel> {
+    const surveyResultCollection = await MongoHelper.getCollection('surveyResults')
+    const res = await surveyResultCollection.findOneAndUpdate({
+      surveyId: data.surveyId,
+      accountId: data.accountId
+    }, {
+      $set: {
+        answer: data.answer,
+        date: data.date
+      }
+    }, {
+      upsert: true, // If not found it will create a new record
+      returnOriginal: false
+    })
+
+    return MongoHelper.map(res.value)
+  }
+}
