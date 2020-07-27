9635809daf7af1d9af8cf690069ca4fe9e6ad18e
georgekaran <georgekaran12@gmail.com>
2020-07-27 00:09:58 -0300

    test: ensure SurveyMongoRepository find survey by id with success

--- src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
@@ -73,4 +73,23 @@ describe('Survey Mongo Repository', () => {
       expect(surveys.length).toBe(0)
     })
   })
+
+  describe('loadById()', () => {
+    test('Should load survey by id on sucess', async () => {
+      const res = await surveyCollection.insertOne({
+        question: 'any_question',
+        answers: [{
+          image: 'any_image',
+          answer: 'any_answer'
+        }],
+        date: new Date()
+      })
+      const id = res.ops[0]._id
+
+      const sut = makeSut()
+      const survey = await sut.loadById(id)
+
+      expect(survey).toBeTruthy()
+    })
+  })
 })
