b80b67c8edeb76e0660eb77f564593c3a8cbefbe
georgekaran <georgekaran12@gmail.com>
2020-08-01 16:59:07 -0300

    docs: add load survey api docs

---
+++ src/main/docs/paths/surveys-path.ts
@@ -0,0 +1,33 @@
+export const surveyPath = {
+  get: {
+    security: [{
+      apiKeyAuth: []
+    }],
+    tags: ['Survey'],
+    summary: 'API for get all surveys',
+    responses: {
+      200: {
+        description: 'Success',
+        content: {
+          'application/json': {
+            schema: {
+              $ref: '#/schemas/surveys'
+            }
+          }
+        }
+      },
+      403: {
+        $ref: '#/components/forbidden'
+      },
+      401: {
+        $ref: '#/components/unauthorized'
+      },
+      404: {
+        $ref: '#/components/notFound'
+      },
+      500: {
+        $ref: '#/components/serverError'
+      }
+    }
+  }
+}
