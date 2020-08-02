4f61eb6c43cc9c7996543ccef430bf390a89bef4
georgekaran <georgekaran12@gmail.com>
2020-08-02 18:39:29 -0300

    feat: ensure loadBySurveyId returns null if there is no survey result

--- src/infra/db/mongodb/survey-result/survey-result-mongo-repository.ts
+++ src/infra/db/mongodb/survey-result/survey-result-mongo-repository.ts
@@ -177,6 +177,6 @@ export class SurveyResultMongoRepository implements SaveSurveyResultRepository, 
       .build()
 
     const surveyResult = await surveyResultCollection.aggregate(query).toArray()
-    return surveyResult?.length ? surveyResult[0] : surveyResult
+    return surveyResult.length ? surveyResult[0] : null
   }
 }
