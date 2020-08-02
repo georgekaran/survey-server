e0b8e19f4c57bb344ada292b9e8f5917063c8177
georgekaran <georgekaran12@gmail.com>
2020-08-02 18:11:18 -0300

    test: ensure load survey route returns 403 if no accessToken is provided

--- src/main/routes/survey-result-routes.test.ts
+++ src/main/routes/survey-result-routes.test.ts
@@ -51,9 +51,7 @@ describe('SurveyResult Routes', () => {
         })
         .expect(403)
     })
-  })
 
-  describe('PUT /surveys/:surveyId/results', () => {
     test('Should return 200 on save survey result with accessToken', async () => {
       const accessToken = await makeAccessToken()
 
@@ -75,6 +73,14 @@ describe('SurveyResult Routes', () => {
           answer: 'Answer 1'
         })
         .expect(200)
+    })
+  })
+
+  describe('GET /surveys/:surveyId/results', () => {
+    test('Should return 403 on load survey result without accessToken', async () => {
+      await request(app)
+        .get('/api/surveys/any_survey_id/results')
+        .expect(403)
     })
   })
 })
