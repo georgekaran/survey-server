33d082493a60bf2c6e00d058a8e5090c11119277
georgekaran <georgekaran12@gmail.com>
2020-08-02 17:29:52 -0300

    feat: ensure LoadSurveyResultController calls LoadSurveyById with correct id

---
+++ src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller-protocols.ts
@@ -0,0 +1,4 @@
+export * from '@/presentation/protocols'
+export * from '@/domain/usecases/survey-result/load-survey-result'
+export * from '@/domain/models/survey-result'
+export * from '@/domain/usecases/survey/load-survey-by-id'
